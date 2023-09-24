e9a2591095e82fbf66663099dbe20cae02eb4ad0
tubi55 <tubi55@nate.com>
2023-09-24 10:44:11 +0900

    addEventListener로 이벤트 연결해야 되는 이유

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const box = document.querySelector('article');
-console.log(box);
+
+console.dir(box);
+
+//box.onclick = () => console.log('엄청 중요한 작업');
+//box.onclick = () => console.log('쓸데없는 작업');
+
+//돔의 이벤트 property에 직접 핸들러함수를 등록하면 추후에 중요핸들러를 덮어쓰기 할수도 있으므로 비권장
+//addEventListener라는 메서드를 활용해서 이벤트 연결
+box.addEventListener('click', () => console.log('엄청중요한작업'));
+box.addEventListener('click', () => console.log('쓸데없는작업'));
