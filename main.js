5d66e91263ff9b3fed1fe7afe2b29a8c72e45351
chojeongeun <juy0816@naver.com>
2023-10-07 10:21:18 +0900

    Object.entries

--- main.js
+++ main.js
@@ -24,3 +24,11 @@ console.log(values);
 
 //map함수와 조합해서 객체의 value값을 치환
 keys.map((el) => console.log(student[el]));
+
+//객체의 key, value를 하나의 배열로 반환
+const result = Object.entries(student);
+console.log(result);
+//배열반환값을 다시 비구조화할당으로 추출 (많이 쓰이는 사례는 아님, 외울필요는 없음 이해만!)
+const [[key, value]] = result;
+console.log(key);
+console.log(value);
